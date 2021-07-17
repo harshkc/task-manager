2fd5ff9baf1620c97872e591799f45e621e7ef5d
harshkc <harsh.hkc1006@gmail.com>
2021-07-17 22:53:08 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div className="Container">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Button from "./Button";
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Title</h1>
+      <Button>Add</Button>
+    </header>
+  );
+};
+
+export default Header;
