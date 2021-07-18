8f08f7ceecfb4ac76ccea6c5296ca1ebf5b3dd64
harshkc <harsh.hkc1006@gmail.com>
2021-07-18 21:26:52 +0530

    Added form UI and actions

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTaskForm from "./components/AddTaskForm";
+import { useState } from "react";
 
 /* Managing state in app js file and 2-way communication is carried with 
       vannila state management, components are interacted with props. */
 
 function App() {
-  const tasks = [
+  const [showAdd, setshowAdd] = useState(true);
+  const [tasks, setTasks] = useState([
     {
-      id: 1,
+      id: 22,
       name: "Doctors Appointment",
       day: "Feb 5th at 2:30pm",
       reminder: true,
     },
     {
-      id: 2,
+      id: 12,
       name: "Meeting at School",
       day: "Feb 6th at 1:30pm",
       reminder: true,
     },
-  ];
+  ]);
+
+  const addTask = (task) => {
+    console.log(task);
+
+    setTasks([...tasks, task]);
+    console.log(tasks);
+  };
+
   return (
     <div className="container">
-      <Header btnText="Add"></Header>
-      <Tasks tasks={tasks}></Tasks>
+      <Header
+        showAdd={showAdd}
+        onbtnClick={() => setshowAdd(!showAdd)}
+      ></Header>
+      {!showAdd && <AddTaskForm onAdd={addTask} />}
+      {tasks.length > 0 ? <Tasks tasks={tasks}></Tasks> : <p>No tasks left</p>}
       <Footer />
     </div>
   );
---
+++ src/components/AddTaskForm.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+const AddTaskForm = ({ onAdd }) => {
+  const [taskName, setTaskName] = useState("");
+  const [taskDateTime, setTaskDateTime] = useState("");
+  const [isReminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!taskName || !taskDateTime) {
+      alert("Don't leave the fields empty");
+      return;
+    }
+
+    onAdd({
+      name: taskName,
+      day: taskDateTime,
+      reminder: isReminder,
+      id: Math.floor(Math.random(1000) * 13) + 1,
+    });
+
+    setTaskName("");
+    setTaskDateTime("");
+    setReminder(false);
+  };
+
+  return (
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label>Task name</label>
+        <input
+          type="text"
+          value={taskName}
+          placeholder="Enter task name"
+          onChange={(e) => setTaskName(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Task date & time</label>
+        <input
+          type="text"
+          value={taskDateTime}
+          placeholder="Enter date and time"
+          onChange={(e) => setTaskDateTime(e.target.value)}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set reminder</label>
+        <input
+          type="checkbox"
+          checked={isReminder}
+          value={isReminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+      <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
+  );
+};
+
+export default AddTaskForm;
